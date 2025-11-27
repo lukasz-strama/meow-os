e9f15f864186c8793ff5641c58adcf5d3d6bfc41
Łukasz Strama <strama.lukasz54@gmail.com>
2025-11-27 21:03:23 +0100

    Handle arrows

--- kernel/drivers/keyboard.c
+++ kernel/drivers/keyboard.c
@@ -7,6 +7,7 @@ static char buffer[256];
 static uint8_t write_ptr = 0;
 static uint8_t read_ptr = 0;
 static bool shift_pressed = false;
+static bool extended = false;
 
 // US QWERTY Lowercase
 char kbd_us_lowercase[] = {
@@ -27,6 +28,12 @@ char kbd_us_uppercase[] = {
 void keyboard_handle() {
     uint8_t scancode = inb(0x60);
 
+    if (scancode == 0xE0) {
+        extended = true;
+        outb(0x20, 0x20);
+        return;
+    }
+
     // Handle Shift Press
     if (scancode == 0x2A || scancode == 0x36) {
         shift_pressed = true;
@@ -43,30 +50,39 @@ void keyboard_handle() {
 
     // If the top bit is set, it's a key release (break code), ignore it
     if (scancode > 0x80) {
+        if (extended) extended = false;
         // Send EOI to Master PIC
         outb(0x20, 0x20);
         return;
     }
 
     char c = 0;
-    // Handle Enter key (0x1C)
-    if (scancode == 0x1C) {
-        c = '\n';
-    } 
-    // Handle Backspace (0x0E)
-    else if (scancode == 0x0E) {
-        c = '\b';
-    }
-    // Handle Escape (0x01)
-    else if (scancode == 0x01) {
-        c = 0x1B;
-    }
-    // Printable characters
-    else if (scancode < 59) {
-        if (shift_pressed) {
-            c = kbd_us_uppercase[scancode];
-        } else {
-            c = kbd_us_lowercase[scancode];
+    if (extended) {
+        extended = false;
+        if (scancode == 0x4B) c = '\x80'; // Left arrow
+        else if (scancode == 0x4D) c = '\x81'; // Right arrow
+        else if (scancode == 0x48) c = '\x82'; // Up arrow
+        else if (scancode == 0x50) c = '\x83'; // Down arrow
+    } else {
+        // Handle Enter key (0x1C)
+        if (scancode == 0x1C) {
+            c = '\n';
+        } 
+        // Handle Backspace (0x0E)
+        else if (scancode == 0x0E) {
+            c = '\b';
+        }
+        // Handle Escape (0x01)
+        else if (scancode == 0x01) {
+            c = 0x1B;
+        }
+        // Printable characters
+        else if (scancode < 59) {
+            if (shift_pressed) {
+                c = kbd_us_uppercase[scancode];
+            } else {
+                c = kbd_us_lowercase[scancode];
+            }
         }
     }
 
