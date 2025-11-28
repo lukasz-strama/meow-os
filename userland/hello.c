c5c0ebf56490bf74d3fcbb57af6a2028f9faa68e
Łukasz Strama <strama.lukasz54@gmail.com>
2025-11-28 14:52:07 +0100

    Implement userland syscall wrapper and enable hello world output

--- userland/hello.c
+++ userland/hello.c
@@ -1,7 +1,11 @@
 #include "lib/syscalls.h"
 
 void main() {
-    // sys_print("Greetings from a standalone binary on Disk!\n");
-    // sys_print("I was loaded via FAT16!\n");
+    sys_print("----------------------------------\n");
+    sys_print("Hello form Userland! (Loaded from Disk)\n");
+    sys_print("Exec works! I am a standalone binary.\n");
+    sys_print("----------------------------------\n");
+
+    // Spin forever so we don't crash returning to nowhere
     while(1);
 }
--- userland/lib/syscalls.h
+++ userland/lib/syscalls.h
@@ -1,4 +1,8 @@
-#pragma once
+#ifndef SYSCALLS_H
+#define SYSCALLS_H
 
 void sys_print(char* msg);
+long syscall1(long number, long arg1);
 void sys_exit();
+
+#endif
